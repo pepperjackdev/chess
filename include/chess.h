87cf7c5fe6cbea01ae3a1fe1aa9450e68ec039b1
pepperjackdev <pepperjackdev@gmail.com>
2026-04-18 22:21:41 +0200

    Writing FEN support

--- include/chess.h
+++ include/chess.h
@@ -3,12 +3,12 @@
 #include <stdint.h>
 
 enum Type {
-  TYPE_PAWN   = 0b00000001,
-  TYPE_ROOK   = 0b00000010,
-  TYPE_KNIGHT = 0b00000011,
-  TYPE_BISHOP = 0b00000100,
-  TYPE_QUEEN  = 0b00000101,
-  TYPE_KING   = 0b00000111
+  TYPE_KING   = 0b00000001,
+  TYPE_QUEEN  = 0b00000010,
+  TYPE_BISHOP = 0b00000011,
+  TYPE_KNIGHT = 0b00000100,
+  TYPE_ROOK   = 0b00000101,
+  TYPE_PAWN   = 0b00000110
 };
 
 enum Side {
@@ -26,10 +26,21 @@ Piece piece_of(PieceType type, PieceSide side) {
   return type | side;
 }
 
-PieceType type_of_piece(Piece piece) {
+PieceType type_of(Piece piece) {
   return piece & 0b00000111;
 }
 
-PieceSide side_of_piece(Piece piece) {
+PieceSide side_of(Piece piece) {
   return piece & 0b00001000;
+}
+
+// FIXME: tmp. implementation 
+void load_fen_into_board(char *fen, Board *board) {
+  // Clearing board
+  for (int i = 0; i < 65; i++) (*board)[i] = 0;
+  // Loading FEN content into board
+  for (int i = 0, bi = 0; fen[i] != '\0'; i++) {
+    char symbol = fen[i];
+    if (symbol == '/') bi += 8;
+  }
 }
--- src/main.c
+++ src/main.c
@@ -1,19 +1,32 @@
 #include <raylib.h>
+#include "chess.h"
 
 typedef enum {
   BOARD,
 } PageType;
 
-typedef union {
+typedef struct {
   PageType pageType;
   void *pageData;
 } Page;
 
 typedef struct {
-  Texture2D sprite;
+  Texture2D *sprite;
   Color blackSquaresColor;
   Color whiteSquaresColor;
+  Board *board;
 } BoardPage;
+
+Rectangle get_piece_sprite(Piece piece) {
+  int row = (side_of(piece) == SIDE_WHITE) ? 0 : 1;
+  int col = type_of(piece) - 1;
+  return (Rectangle){
+    col * 320,
+    row * 320,
+    320,
+    320,
+  };
+};
 
 void draw_board_page(BoardPage *boardPage) {
   float squareWidth = GetScreenWidth() / 8.0f;
@@ -26,17 +39,37 @@ void draw_board_page(BoardPage *boardPage) {
           col * squareWidth,
           row * squareHeight,
           squareWidth,
-          squareHeight,
-          ((row + col) % 2 == 0) ? boardPage->blackSquaresColor : boardPage->whiteSquaresColor
+          squareHeight, 
+          ((row + col) % 2 == 0) ? 
+            boardPage->blackSquaresColor : 
+            boardPage->whiteSquaresColor
+      );
+
+      // Pieces
+      Piece piece = (*boardPage->board)[row * 8 + col];
+      if (piece == 0) continue;
+
+      DrawTexturePro(
+        *boardPage->sprite, 
+        get_piece_sprite(piece), 
+        (Rectangle){
+          col * squareWidth,
+          row * squareHeight,
+          squareWidth,
+          squareHeight
+        },
+        (Vector2){0, 0}, 
+        0.0f, 
+        RAYWHITE
       );
     }
   }
 }
 
-void draw_page(Page *page) {
-  switch (page->pageType) {
+void draw_page(Page page) {
+  switch (page.pageType) {
     case BOARD:
-      draw_board_page((BoardPage*)page->pageData);
+      draw_board_page((BoardPage*)page.pageData);
       break;
   }
 }
@@ -46,9 +79,37 @@ int main() {
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(960, 960, "Chess");
 
+  Image spriteImage = LoadImage("assets/sprites/chess_pieces_sprite.png");
+  Texture2D spriteTexture = LoadTextureFromImage(spriteImage);
+  UnloadImage(spriteImage);
+
+  Board board = {
+    piece_of(TYPE_PAWN, SIDE_WHITE), 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 0, piece_of(TYPE_BISHOP, SIDE_BLACK), 0, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0
+  };
+
+  BoardPage boardPage = {
+    &spriteTexture,
+    (Color){0xDA, 0xD7, 0xCD, 0xFF},
+    (Color){0xA1, 0x87, 0x68, 0xFF},
+    &board
+  };
+
+  Page page = {
+    BOARD,
+    &boardPage
+  };
+
   while (!WindowShouldClose()) {
     BeginDrawing();
       ClearBackground(RAYWHITE);
+      draw_page(page);
     EndDrawing();
   }
 
