fae1207ab28ab9e22c98a5a775e67499d2027e38
pepperjackdev <pepperjackdev@gmail.com>
2026-05-01 22:04:09 +0200

    Fixed implementation of load_fen_into_board

--- src/chess.c
+++ src/chess.c
@@ -1,76 +1,74 @@
 #include "chess.h"
 
-#include <raylib.h>
+#include <string.h>
 #include <stdio.h>
 
-Piece piece_of(PieceType type, PieceSide side) { 
-  return type | side; 
+Piece piece_of(PieceType type, PieceSide side) {
+    return type | side;
 }
 
-PieceType type_of(Piece piece) { 
-  return piece & 0b00000111; 
+PieceType type_of(Piece piece) {
+    return piece & 0b00000111;
 }
 
-PieceSide side_of(Piece piece) { 
-  return piece & 0b00001000; 
+PieceSide side_of(Piece piece) {
+    return piece & SIDE_BLACK;
 }
 
-// FIXME: tmp. implementation
 void load_fen_into_board(char *fen, Board *board) {
-  // Clearing board
-  for (int i = 0; i < 65; i++) {
-(*board)[i] = 0;
-  }
-  // Loading FEN content into board
-  for (int fen_index = 0, board_index = 0; 
-      fen[fen_index] != '\0' && board_index < 64; 
-      fen_index++) {
-    char symbol = fen[fen_index];
+    if (fen == NULL || board == NULL) return;
+    // Clearing the Board
+    memset(*board, 0, 64);
     
-    if (symbol == '/') continue;
-    
-    if (symbol >= '1' && symbol <= '8') {
-      board_index += symbol - '0';
-      continue;
-    }
-    
-    PieceSide side;
-    if (symbol >= 'A' && symbol <= 'Z') {
-      side = SIDE_WHITE;
-    } else if (symbol >= 'a' && symbol <= 'z') {
-      side = SIDE_BLACK;
-    }
+    // Parsing
+    int rank = 0;
+    int file = 0;
 
-    PieceType type;
-    switch (symbol) {
-      case 'K':
-      case 'k':
-        type = TYPE_KING;
-        break;
-      case 'Q':
-      case 'q':
-        type = TYPE_QUEEN;
-        break;
-      case 'B':
-      case 'b':
-        type = TYPE_BISHOP;
-        break;
-      case 'N':
-      case 'n':
-        type = TYPE_KNIGHT;
-        break;
-      case 'R':
-      case 'r':
-        type = TYPE_ROOK;
-        break;
-      case 'P':
-      case 'p':
-        type = TYPE_PAWN;
-        break;
-      default:
-        fprintf(stderr, "Invalid symbol");
-    }
+    for (int i = 0; fen[i] != '\0' && fen[i] != ' '; i++) {
+        char symbol = fen[i];
+        // End of Rank
+        if (symbol == '/') {
+            rank++;
+            file = 0;
+            continue;
+        }
+        // Empty spaces
+        if (symbol >= '1' && symbol <= '8') {
+            file += symbol - '0';
+            continue;
+        }
 
-    (*board)[board_index++] = piece_of(type, side);
-  }
-} 
+        // Piece
+        PieceType type = 0;
+        PieceSide side = SIDE_WHITE;
+
+        switch (symbol) {
+            case 'P': side = SIDE_WHITE; type = TYPE_PAWN;   break;
+            case 'N': side = SIDE_WHITE; type = TYPE_KNIGHT; break;
+            case 'B': side = SIDE_WHITE; type = TYPE_BISHOP; break;
+            case 'R': side = SIDE_WHITE; type = TYPE_ROOK;   break;
+            case 'Q': side = SIDE_WHITE; type = TYPE_QUEEN;  break;
+            case 'K': side = SIDE_WHITE; type = TYPE_KING;   break;
+
+            case 'p': side = SIDE_BLACK; type = TYPE_PAWN;   break;
+            case 'n': side = SIDE_BLACK; type = TYPE_KNIGHT; break;
+            case 'b': side = SIDE_BLACK; type = TYPE_BISHOP; break;
+            case 'r': side = SIDE_BLACK; type = TYPE_ROOK;   break;
+            case 'q': side = SIDE_BLACK; type = TYPE_QUEEN;  break;
+            case 'k': side = SIDE_BLACK; type = TYPE_KING;   break;
+
+            default:
+                fprintf(stderr, 
+                    "Warning: Invalid FEN character '%c'\n", 
+                    symbol);
+                continue;
+        }
+
+        // Loading determied piece into board
+        if (type != 0 && file < 8) {
+            int square = rank * 8 + file;
+            (*board)[square] = piece_of(type, side);
+            file++;
+        }
+    }
+}
